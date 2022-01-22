efef7edbf25bc02a45a6b97b6e4d0644ae383d67
Stepan <bai_777@mail.ru>
2022-01-22 20:29:44 +0300

    создал AppComponent.kt

--- app/src/main/java/com/example/translator/di/AppComponent.kt
+++ app/src/main/java/com/example/translator/di/AppComponent.kt
@@ -1,4 +1,31 @@
 package com.example.translator.di
 
+import android.app.Application
+import com.example.translator.application.TranslatorApp
+import dagger.BindsInstance
+import dagger.Component
+import dagger.android.support.AndroidSupportInjection
+import javax.inject.Singleton
+
+@Component(
+    modules = [
+        InteractorModule::class,
+        RepositoryModule::class,
+        ViewModelModule::class,
+        ActivityModule::class,
+        AndroidSupportInjection::class
+    ]
+)
+
+@Singleton
 interface AppComponent {
+    @Component.Builder
+    interface Builder {
+        @BindsInstance
+        fun application(application: Application): Builder
+
+        fun build(): AppComponent
+    }
+
+    fun inject(englishVocabularyApp: TranslatorApp)
 }
