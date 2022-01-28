67ac3a5c9c5fd04bb684595938990a610a4036f5
Stepan <bai_777@mail.ru>
2022-01-28 19:01:34 +0300

    изменил класс RoomDataBaseImplementation.kt

--- app/src/main/java/com/example/translator/model/datasource/RoomDataBaseImplementation.kt
+++ app/src/main/java/com/example/translator/model/datasource/RoomDataBaseImplementation.kt
@@ -2,9 +2,10 @@ package com.example.translator.model.datasource
 
 import com.example.translator.model.data.DataModel
 import io.reactivex.Observable
+import kotlinx.coroutines.Deferred
 
 class RoomDataBaseImplementation: DataSource<List<DataModel>> {
-    override suspend fun getData(word: String): Observable<List<DataModel>> {
+    override suspend fun getData(word: String): List<DataModel> {
         TODO("Not yet implemented")
     }
 }
