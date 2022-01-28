1fe95cc3d148f5ae121de3e9aafffc3e165e7278
Stepan <bai_777@mail.ru>
2022-01-28 19:03:29 +0300

    изменил интерфейсы DataSource.kt и Repository.kt

--- app/src/main/java/com/example/translator/model/datasource/DataSource.kt
+++ app/src/main/java/com/example/translator/model/datasource/DataSource.kt
@@ -1,7 +1,5 @@
 package com.example.translator.model.datasource
 
-import com.example.translator.model.data.DataModel
-
 interface DataSource<T> {
-    suspend fun getData(word: String): List<DataModel>
+    suspend fun getData(word: String): T
 }
--- app/src/main/java/com/example/translator/model/datasource/RoomDataBaseImplementation.kt
+++ app/src/main/java/com/example/translator/model/datasource/RoomDataBaseImplementation.kt
@@ -1,8 +1,6 @@
 package com.example.translator.model.datasource
 
 import com.example.translator.model.data.DataModel
-import io.reactivex.Observable
-import kotlinx.coroutines.Deferred
 
 class RoomDataBaseImplementation: DataSource<List<DataModel>> {
     override suspend fun getData(word: String): List<DataModel> {
