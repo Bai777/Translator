3730371ca0e7ec8780a74cbc8128b1953fb68ab3
Stepan <bai_777@mail.ru>
2022-02-13 22:19:42 +0300

    изменил MainInteractor.kt

--- app/src/main/java/com/example/translator/view/main/MainInteractor.kt
+++ app/src/main/java/com/example/translator/view/main/MainInteractor.kt
@@ -1,20 +1,23 @@
 package com.example.translator.view.main
 
 import com.example.model.data.AppState
-import com.example.model.data.userdata.DataModel
+import com.example.model.data.dto.SearchResultDto
+import com.example.repository.Repository
+import com.example.repository.RepositoryLocal
+import com.example.translator.utils.mapSearchResultToResult
 
 class MainInteractor(
-    private val repositoryRemote: com.example.repository.Repository<List<DataModel>>,
-    private val repositoryLocal: com.example.repository.RepositoryLocal<List<DataModel>>,
+    private val repositoryRemote: Repository<List<SearchResultDto>>,
+    private val repositoryLocal: RepositoryLocal<List<SearchResultDto>>,
 ) : com.example.core.viewmodel.Interactor<AppState> {
 
     override suspend fun getData(word: String, fromRemoteSource: Boolean): AppState {
         val appState: AppState
         if (fromRemoteSource) {
-            appState = AppState.Success(repositoryRemote.getData(word))
+            appState = AppState.Success(mapSearchResultToResult(repositoryRemote.getData(word)))
             repositoryLocal.saveToDB(appState)
         } else {
-            appState = AppState.Success(repositoryLocal.getData(word))
+            appState = AppState.Success(mapSearchResultToResult(repositoryLocal.getData(word)))
         }
         return appState
     }
